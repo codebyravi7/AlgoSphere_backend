c7410dbe982e3ee1f33ce835b262b289e7b71ddb
Ravikant <ravidreamer7@gmail.com>
2024-09-13 00:18:48 +0530

    "cookies"

--- middleware/protectRoute.js
+++ middleware/protectRoute.js
@@ -3,7 +3,7 @@ import { User } from "../models/user.model.js";
 
 const protectRoute = async (req, res, next) => {
   try {
-    const token = req.cookies.jwt;
+    const token = req.cookies.jwt || req.cookies._vercel_jwt;
     
     if (!token) {
       return res
